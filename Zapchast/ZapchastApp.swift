7d39005b32c99a390a4845315d203778f8472290
Urmat Asanaliev <forgithubasanalievurmat10@gmail.com>
2024-09-08 12:54:29 +0600

    Initial Commit

---
+++ Zapchast/ZapchastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZapchastApp.swift
+//  Zapchast
+//
+//  Created by Урмат Асаналиев on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ZapchastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
